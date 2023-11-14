a3269cd615d39c86f1fabe5969e13c3499371dc6
oddbjorn.lona@skuld.com <oddbjorn.lona@skuld.com>
2023-11-14 21:09:47 +0100

    Initial Commit after running project startup

---
+++ android/src/main/java/com/axehigh/game/poc/menu/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.axehigh.game.poc.menu.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.axehigh.game.poc.menu.game;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new game(), configuration);
+    }
+}
